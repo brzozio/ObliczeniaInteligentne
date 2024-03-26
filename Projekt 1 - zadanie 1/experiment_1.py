2dda4c67a1c14e55f824570c93ce13c15524bbf4
brzozio <michal.brzozowski@interia.eu>
2024-03-27 00:08:50 +0100

    Added experiment 1 partially

--- Projekt 1 - zadanie 1/experiment_1.py
+++ Projekt 1 - zadanie 1/experiment_1.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
-from warmup import load as load_csv
+from sklearn import cluster
+import matplotlib.pyplot as plt
+from sklearn.metrics import silhouette_score
+from warmup import plot_voronoi_diagram
+from main import labels, points
 
 """
 Za pomocą technik klasteryzacji można sprawdzić czy obiekty w zbiorze danych są rozłożone w przestrzeni równomiernie, czy też występują w nich rozdzielone skupiska obiektów podobnych. 
@@ -19,6 +23,21 @@ Liczba znalezionych w ten sposób skupisk zależy od struktury analizowanego zbi
 Należy zwrócić uwagę, że metoda ta umożliwia również wykrywanie wyjątków (ang. outliers), jednak w niniejszym zadaniu nie należy z tego korzystać przyjmując wartość parametru min_samples równą 1. 
 Reszta parametrów może mieć domyślne wartości.
 """
+def experiment_1_KMeans() -> None:
+    for index in range(6):
+        for n_clusters in range(1,9):
+            #K-Means cluster
+            klaster_KMeans: cluster.KMeans = cluster.KMeans(n_clusters=n_clusters)
+            klaster_KMeans.fit(points[index])
+            y_pred = klaster_KMeans.labels_.astype(int)
+            plot_voronoi_diagram(points[index], labels[index], y_pred)
 
-if __name__ == "__main__":
+            #Silhouette Score
+            sil_score_kmeans : float = silhouette_score(points[index], labels[index])
+            plt.plot(n_clusters, sil_score_kmeans, 'ro', label=str(labels[index]))
+            plt.title(f'CSV: {index}, {n_clusters}-CLUSTERS')
+            plt.xlabel("n-clusters")
+            plt.ylabel("Silhouette Score")
+
+def experiment_1_DBSCAN() -> None:
     pass
