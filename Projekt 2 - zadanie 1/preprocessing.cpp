c27d597027468ef3c357f50f8d8a050dde3d70f5
NorbertSmiechowicz <norbert.smiechowicz@gmail.com>
2024-05-08 17:08:54 +0200

    refactor funkcji, lekkie uogólnienie do danych

--- Projekt 2 - zadanie 1/preprocessing.cpp
+++ Projekt 2 - zadanie 1/preprocessing.cpp
@@ -40,9 +40,8 @@ static void write_standardized_data(std::ofstream* output_file, std::vector<std:
 }
 
 static std::vector<std::vector<float>> standard_mask_processing(std::ofstream* output_file,
-	std::vector<unsigned char>* data, std::vector<std::vector<float>>* masks,
-	std::vector<float>* masks_id_sum, int dim_data, bool is_training,
-	std::vector<std::vector<float>>* in_std_distr) {
+	std::vector<uint8_t>* data, std::vector<std::vector<float>>* masks,
+	int dim_data, bool is_training, std::vector<std::vector<float>>* in_std_distr) {
 
 	int data_iterator = 0;
 	int current_id_sum_int = 0;
@@ -66,7 +65,7 @@ static std::vector<std::vector<float>> standard_mask_processing(std::ofstream* o
 				current_convolve += (float)(*data)[data_iterator] * (*masks)[_mask][_component];
 				data_iterator++;
 			}
-			processed_data[_data_point][_mask] = current_convolve / current_id_sum_float / (*masks_id_sum)[_mask];
+			processed_data[_data_point][_mask] = current_convolve / current_id_sum_float;
 			data_iterator -= dim_data;
 		}
 		data_iterator += dim_data;
@@ -83,36 +82,36 @@ static std::vector<std::vector<float>> standard_mask_processing(std::ofstream* o
 }
 
 static std::vector<std::vector<float>> standard_differential_processing(std::ofstream* output_file,
-	std::vector<unsigned char>* data, int dim_data, bool is_training,
+	std::vector<uint8_t>* data, int side_len, bool is_training,
 	std::vector<std::vector<float>>* in_std_distr) {
 
-	std::vector<std::vector<float>> differential_data((*data).size() / dim_data / dim_data,
-		std::vector<float>(dim_data*2));
+	std::vector<std::vector<float>> differential_data((*data).size() / side_len / side_len,
+		std::vector<float>(side_len*2));
 
 	int scaled_y;
 
 	for (int _data_point = 0; _data_point < differential_data.size(); _data_point++) {
-		scaled_y = _data_point * dim_data * dim_data;
-		for (int _y_component = 0; _y_component < dim_data; _y_component++) {
-			for (int _x_component = 0; _x_component < dim_data - 1; _x_component++) {
+		scaled_y = _data_point * side_len * side_len;
+		for (int _y_component = 0; _y_component < side_len; _y_component++) {
+			for (int _x_component = 0; _x_component < side_len - 1; _x_component++) {
 				if (((*data)[scaled_y + _x_component + 1] == 0) xor
 					((*data)[scaled_y + _x_component] == 0)) {
-					differential_data[_data_point][_y_component + dim_data]++;
+					differential_data[_data_point][_y_component + side_len]++;
 				}
 			}
-			scaled_y += dim_data;
+			scaled_y += side_len;
 		}
 	}
 
 	for (int _data_point = 0; _data_point < differential_data.size(); _data_point++) {
-		for (int _x_component = 0; _x_component < dim_data; _x_component++) {
-			scaled_y = _data_point * dim_data * dim_data;
-			for (int _y_component = 0; _y_component < dim_data - 1; _y_component++) {
-				if (((*data)[scaled_y + _x_component + dim_data] == 0) xor
+		for (int _x_component = 0; _x_component < side_len; _x_component++) {
+			scaled_y = _data_point * side_len * side_len;
+			for (int _y_component = 0; _y_component < side_len - 1; _y_component++) {
+				if (((*data)[scaled_y + _x_component + side_len] == 0) xor
 					((*data)[scaled_y + _x_component] == 0)) {
 					differential_data[_data_point][_x_component]++;
 				}
-				scaled_y += dim_data;
+				scaled_y += side_len;
 			}
 		}
 	}
@@ -127,52 +126,17 @@ static std::vector<std::vector<float>> standard_differential_processing(std::ofs
 	return (*in_std_distr);
 }
 
-int main() {
+static std::vector<std::vector<float>> gen_digit_masks(std::vector<uint8_t>* data,
+	std::vector<uint8_t>* target, int dim_data, int size_target) {
 
-	constexpr int size_target_header = 8;
-	constexpr int size_data_header = 16;
-	constexpr int size_target = 10;
-
-	// loading data
-
-	std::ifstream input_train_data("train-images-idx3-ubyte", std::ios::binary);
-	if (!input_train_data.is_open()) {
-		std::cout << "unable to open source file";
-		return -1;
-	}
-	std::vector<unsigned char> train_data(std::istreambuf_iterator<char>(input_train_data), {});
-	input_train_data.close();
-	train_data.erase(train_data.begin(), std::next(train_data.begin(), size_data_header));
-
-	std::ifstream input_test_data("t10k-images-idx3-ubyte", std::ios::binary);
-	if (!input_test_data.is_open()) {
-		std::cout << "unable to open source file";
-		return -1;
-	}
-	std::vector<unsigned char> test_data(std::istreambuf_iterator<char>(input_test_data), {});
-	input_test_data.close();
-	test_data.erase(test_data.begin(), std::next(test_data.begin(), size_data_header));
-
-	std::ifstream input_labels("train-labels-idx1-ubyte", std::ios::binary);
-	if (!input_labels.is_open()) {
-		std::cout << "unable to open source file";
-		return -1;
-	}
-	std::vector<unsigned char> target(std::istreambuf_iterator<char>(input_labels), {});
-	input_labels.close();
-	target.erase(target.begin(), std::next(target.begin(), size_target_header));
-
-	int dim_data = train_data.size() / target.size();
 	std::vector<std::vector<float>> masks(size_target, std::vector<float>(dim_data));
-
-	// mask generation
 
 	int current_label = 0;
 	int data_iterator = 0;
-	for (int _data_point = 0; _data_point < target.size(); _data_point++) {
-		current_label = target[_data_point];
+	for (int _data_point = 0; _data_point < (*target).size(); _data_point++) {
+		current_label = (*target)[_data_point];
 		for (int _component = 0; _component < dim_data; _component++) {
-			masks[current_label][_component] += (float)train_data[data_iterator];
+			masks[current_label][_component] += (float)(*data)[data_iterator];
 			data_iterator++;
 		}
 	}
@@ -183,7 +147,50 @@ int main() {
 		for (int _component = 0; _component < dim_data; _component++) {
 			masks_id_sum[_mask] += masks[_mask][_component];
 		}
+		for (int _component = 0; _component < dim_data; _component++) {
+			masks[_mask][_component] /= masks_id_sum[_mask];
+		}
 	}
+
+	return masks;
+}
+
+
+int main() {
+
+	// loading data
+
+	constexpr int size_target_header = 8;
+	constexpr int size_data_header = 16;
+	constexpr int size_target = 10;
+
+	std::ifstream input_train_data("train-images-idx3-ubyte", std::ios::binary);
+	if (!input_train_data.is_open()) {
+		std::cout << "unable to open source file";
+		return -1;
+	}
+	std::vector<uint8_t> train_data(std::istreambuf_iterator<char>(input_train_data), {});
+	input_train_data.close();
+	train_data.erase(train_data.begin(), std::next(train_data.begin(), size_data_header));
+
+	std::ifstream input_test_data("t10k-images-idx3-ubyte", std::ios::binary);
+	if (!input_test_data.is_open()) {
+		std::cout << "unable to open source file";
+		return -1;
+	}
+	std::vector<uint8_t> test_data(std::istreambuf_iterator<char>(input_test_data), {});
+	input_test_data.close();
+	test_data.erase(test_data.begin(), std::next(test_data.begin(), size_data_header));
+
+	std::ifstream input_labels("train-labels-idx1-ubyte", std::ios::binary);
+	if (!input_labels.is_open()) {
+		std::cout << "unable to open source file";
+		return -1;
+	}
+	std::vector<uint8_t> target(std::istreambuf_iterator<char>(input_labels), {});
+	input_labels.close();
+	target.erase(target.begin(), std::next(target.begin(), size_target_header));
+	int dim_data = train_data.size() / target.size();
 
 	// data processing
 
@@ -196,13 +203,16 @@ int main() {
 	standard_differential_processing(&output_diff_test_data, &test_data, 28, false, &diff_train_std_distr);
 	output_diff_test_data.close();
 
+	std::vector<std::vector<float>> digit_masks(size_target, std::vector<float>(dim_data));
+	digit_masks = gen_digit_masks(&train_data, &target, dim_data, size_target);
+
 	std::ofstream output_mask_train_data("mean_digit_convolution_train_data.txt");
 	std::vector<std::vector<float>> mask_train_std_distr;
-	mask_train_std_distr = standard_mask_processing(&output_mask_train_data, &train_data, &masks, &masks_id_sum, dim_data, true, {});
+	mask_train_std_distr = standard_mask_processing(&output_mask_train_data, &train_data, &digit_masks, dim_data, true, {});
 	output_mask_train_data.close();
 
 	std::ofstream output_mask_test_data("mean_digit_convolution_test_data.txt");
-	standard_mask_processing(&output_mask_test_data, &test_data, &masks, &masks_id_sum, dim_data, false, &mask_train_std_distr);
+	standard_mask_processing(&output_mask_test_data, &test_data, &digit_masks, dim_data, false, &mask_train_std_distr);
 	output_mask_test_data.close();
 
 	return 0;
