9a63a7a991a9aa4ddd3cb147fdc88747f2d19e23
brzozio <michal.brzozowski@interia.eu>
2024-06-01 20:41:17 +0200

    sss

--- Projekt 3/main.py
+++ Projekt 3/main.py
@@ -36,13 +36,13 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import torch
-from captum.attr import FeatureAblation
 from captum.attr import visualization as viz
 from captum.attr import IntegratedGradients
 from captum.attr import Saliency
 from captum.attr import LayerGradCam
 from captum.attr import Lime
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model_CNN_mnist         = CNN_tanh(in_side_len=28, in_channels=1, cnv0_out_channels=12, cnv1_out_channels=16, lin0_out_size=100, lin1_out_size=10, convolution_kernel=3, pooling_kernel=2, reduce_to_dim2=False)
 model_CNN_cifar         = CNN_tanh(in_side_len=32, in_channels=3, cnv0_out_channels=15, cnv1_out_channels=16, lin0_out_size=128, lin1_out_size=10, convolution_kernel=7, pooling_kernel=2, reduce_to_dim2=False)
@@ -51,27 +51,26 @@ model_MLP_iris          = MLP(input_size=4, hidden_layer_size=3, classes=3)
 model_MLP_wine          = MLP(input_size=13, hidden_layer_size=7, classes=3)
 model_MLP_breast_cancer = MLP(input_size=30, hidden_layer_size=15, classes=2)
 
-model_MLP_mnist_conv         = MLP(input_size=10, hidden_layer_size=40, classes=10)
-model_MLP_mnist_diff         = MLP(input_size=10, hidden_layer_size=84, classes=10)
+model_MLP_mnist_conv    = MLP(input_size=10, hidden_layer_size=40, classes=10)
+model_MLP_mnist_diff    = MLP(input_size=10, hidden_layer_size=84, classes=10)
 
-data_MLP_iris  = datasets_get.iris()
-data_MLP_wine  = datasets_get.wine()
-data_MLP_breast_cancer  = datasets_get.breast_cancer()
-data_MLP_mnist_conv = datasets_get.mnist_extr_conv()
-data_MLP_mnist_diff = datasets_get.mnist_extr_diff()
-
-data_CNN_mnist = datasets_get.mnist_to_cnn()
-data_CNN_cifar = datasets_get.cifar10_to_cnn()
+data_MLP_iris           = datasets_get.iris(device)
+data_MLP_wine           = datasets_get.wine(device)
+data_MLP_breast_cancer  = datasets_get.breast_cancer(device)
+#data_MLP_mnist_conv     = datasets_get.mnist_extr_conv(device, True, True)
+#data_MLP_mnist_diff     = datasets_get.mnist_extr_diff(device, True, True)
+data_CNN_mnist          = datasets_get.mnist_to_cnn(device, True)
+data_CNN_cifar          = datasets_get.cifar10_to_cnn(device, True)
 
 
 def loading_state_dict():
-    model_CNN_mnist.load_state_dict('./models/CNN_mnist.pth')
-    model_CNN_cifar.load_state_dict('./models/CNN_cifar.pth')
-    model_MLP_iris.load_state_dict('./models/MLP_iris.pth')
-    model_MLP_wine.load_state_dict('./models/MLP_wine.pth')
-    model_MLP_breast_cancer.load_state_dict('./models/MLP_breast_cancer.pth')
-    model_MLP_mnist_conv.load_state_dict('./models/MLP_mnist_extr_conv.pth')
-    model_MLP_mnist_diff.load_state_dict('./models/MLP_mnist_extr_diff.pth')
+    model_CNN_mnist.load_state_dict(torch.load('./models/CNN_mnist.pth'))
+    model_CNN_cifar.load_state_dict(torch.load('./models/CNN_cifar.pth'))
+    model_MLP_iris.load_state_dict(torch.load('./models/MLP_iris.pth'))
+    model_MLP_wine.load_state_dict(torch.load('./models/MLP_wine.pth'))
+    model_MLP_breast_cancer.load_state_dict(torch.load('./models/MLP_breast_cancer.pth'))
+    model_MLP_mnist_conv.load_state_dict(torch.load('./models/MLP_mnist_extr_conv.pth'))
+    model_MLP_mnist_diff.load_state_dict(torch.load('./models/MLP_mnist_extr_diff.pth'))
 
 def explain_lime(model, data, target, feature_names):
     model.eval()
@@ -108,5 +107,6 @@ def explain_integrated_gradients(model, data, target, baseline=None):
 
 if __name__ == "__main__":
     loading_state_dict()
+    explain_gradCAM(model=model_CNN_cifar, data=data_CNN_cifar.data[1], target=data_CNN_cifar.targets[1], layer=model_CNN_cifar.conv0)
 
     
