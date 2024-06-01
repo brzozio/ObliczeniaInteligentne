584659fc7b2d48909d8430ce65c9040f5d5649b9
brzozio <michal.brzozowski@interia.eu>
2024-06-01 13:08:45 +0200

    Created Project 3 files

---
+++ Projekt 3/model_MLP.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import torch.nn as nn
+
+
+class MLP(nn.Module):
+    def __init__(self, input_size, hidden_layer_size, classes) -> None:
+        super(MLP, self).__init__()
+
+        self.lay_1        = nn.Linear(input_size, hidden_layer_size)
+        self.activation   = nn.ReLU()
+       #self.activation   = nn.Tanh()
+        self.lay_2        = nn.Linear(hidden_layer_size, classes)
+
+    def forward(self, x):
+        out = self.lay_1(x)
+        out = self.activation(out)
+        out = self.lay_2(out)
+        return out
