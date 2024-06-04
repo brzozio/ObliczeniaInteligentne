5cfc99147dcbe3e53ba078b8a0565699a407cfcd
brzozio <michal.brzozowski@interia.eu>
2024-06-04 20:08:25 +0200

    working mdoels, need to rework the gradCAM and other captum explainers

--- Projekt 3/main.py
+++ Projekt 3/main.py
@@ -41,11 +41,21 @@ from captum.attr import IntegratedGradients
 from captum.attr import Saliency
 from captum.attr import LayerGradCam
 from captum.attr import Lime
+import seaborn as sb
+from sklearn.metrics import confusion_matrix, accuracy_score
+import torch.nn as nn
+import torch.optim as optim
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model_CNN_mnist         = CNN_tanh(in_side_len=28, in_channels=1, cnv0_out_channels=12, cnv1_out_channels=16, lin0_out_size=100, lin1_out_size=10, convolution_kernel=3, pooling_kernel=2, reduce_to_dim2=False)
-model_CNN_cifar         = CNN_tanh(in_side_len=32, in_channels=3, cnv0_out_channels=15, cnv1_out_channels=16, lin0_out_size=128, lin1_out_size=10, convolution_kernel=7, pooling_kernel=2, reduce_to_dim2=False)
+model_CNN_mnist         = CNN_tanh(in_side_len=28, in_channels=1, cnv0_out_channels=12, 
+                                   cnv1_out_channels=16, lin0_out_size=100, lin1_out_size=10, 
+                                   convolution_kernel=3, pooling_kernel=2, reduce_to_dim2=False)
+
+model_CNN_cifar         = CNN_tanh(in_side_len=32, in_channels=3, cnv0_out_channels=15, 
+                                   cnv1_out_channels=16, lin0_out_size=128, lin1_out_size=10, 
+                                   convolution_kernel=7, pooling_kernel=2, reduce_to_dim2=False)
 
 model_MLP_iris          = MLP(input_size=4, hidden_layer_size=3, classes=3)
 model_MLP_wine          = MLP(input_size=13, hidden_layer_size=7, classes=3)
@@ -57,20 +67,59 @@ model_MLP_mnist_diff    = MLP(input_size=10, hidden_layer_size=84, classes=10)
 data_MLP_iris           = datasets_get.iris(device)
 data_MLP_wine           = datasets_get.wine(device)
 data_MLP_breast_cancer  = datasets_get.breast_cancer(device)
-#data_MLP_mnist_conv     = datasets_get.mnist_extr_conv(device, True, True)
-#data_MLP_mnist_diff     = datasets_get.mnist_extr_diff(device, True, True)
+data_MLP_mnist_conv     = datasets_get.mnist_extr_conv(device, False, 'test')
+data_MLP_mnist_diff     = datasets_get.mnist_extr_diff(device, False, 'test')
 data_CNN_mnist          = datasets_get.mnist_to_cnn(device, True)
 data_CNN_cifar          = datasets_get.cifar10_to_cnn(device, True)
 
 
+def execute_model(data_set, model, data_name):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f'CUDA VERSION: {torch.version.cuda}')
+    print(f'DEVICE RUNING: {device}')
+
+
+    model.load_state_dict(torch.load(f'./Projekt 3/models/{data_name}.pth'))
+    model.eval()
+    model.double()
+    model.to(device)
+
+    outputs = model.extract(data_set.data)
+    outputs = model.forward(outputs)
+    print(f"OUTPUS: {outputs}")
+    
+    softmax = torch.nn.Softmax(dim=1)
+    probabilities = softmax(outputs)
+    predicted_classes = torch.argmax(probabilities, dim=1)
+
+    print(f'PREDICTED CLASSES: {predicted_classes}')
+    print(f"ORIGINAL CLASSES: {data_set.targets}")
+
+
+    plt.figure(figsize=(10, 7))        
+    
+    predicted_classes_cpu = predicted_classes.cpu().numpy()
+    targets_cpu           = data_set.targets.cpu().numpy()
+    
+    sb.heatmap(confusion_matrix(targets_cpu,predicted_classes_cpu), annot=True, cmap='Blues', fmt='g')
+    plt.xlabel('Predicted labels')
+    plt.ylabel('True labels')
+    plt.title(f"Conf Matrix - {data_name}")
+    plt.show()
+    
+    accuracy = accuracy_score(predicted_classes_cpu, targets_cpu)
+    print(f'ACCURACY SCORE FOR {data_name}: {accuracy:.4f}')
+
+        
+
 def loading_state_dict():
-    model_CNN_mnist.load_state_dict(torch.load('./models/CNN_mnist.pth'))
-    model_CNN_cifar.load_state_dict(torch.load('./models/CNN_cifar.pth'))
-    model_MLP_iris.load_state_dict(torch.load('./models/MLP_iris.pth'))
-    model_MLP_wine.load_state_dict(torch.load('./models/MLP_wine.pth'))
-    model_MLP_breast_cancer.load_state_dict(torch.load('./models/MLP_breast_cancer.pth'))
-    model_MLP_mnist_conv.load_state_dict(torch.load('./models/MLP_mnist_extr_conv.pth'))
-    model_MLP_mnist_diff.load_state_dict(torch.load('./models/MLP_mnist_extr_diff.pth'))
+    model_CNN_mnist.load_state_dict(torch.load('./Projekt 3/models/CNN_mnist.pth'))
+    model_CNN_cifar.load_state_dict(torch.load('./Projekt 3/models/CNN_cifar.pth'))
+    #model_MLP_iris.load_state_dict(torch.load('./Projekt 3/models/MLP_iris.pth'))
+    model_MLP_wine.load_state_dict(torch.load('./Projekt 3/models/MLP_wine.pth'))
+    model_MLP_breast_cancer.load_state_dict(torch.load('./Projekt 3/models/MLP_breast_cancer.pth'))
+    model_MLP_mnist_conv.load_state_dict(torch.load('./Projekt 3/models/MLP_mnist_extr_conv.pth'))
+    #model_MLP_mnist_diff.load_state_dict(torch.load('./Projekt 3/models/MLP_mnist_extr_diff.pth'))
 
 def explain_lime(model, data, target, feature_names):
     model.eval()
@@ -81,6 +130,8 @@ def explain_lime(model, data, target, feature_names):
     viz.visualize_text([attr], feature_names, title="LIME Attribution")
 
 def explain_gradCAM(model, data, target, layer):
+    model.double()
+    model.to(device)
     model.eval()
     gradcam = LayerGradCam(model, layer)
     attr = gradcam.attribute(data, target=target)
@@ -89,6 +140,8 @@ def explain_gradCAM(model, data, target, layer):
     viz.visualize_image_attr(attr, data.permute(1, 2, 0).numpy(), method="heat_map", title="GradCAM Attribution")
 
 def explain_saliency(model, data, target):
+    model.double()
+    model.to(device)
     model.eval()
     saliency = Saliency(model)
     attr = saliency.attribute(data, target=target, abs=True)
@@ -97,6 +150,8 @@ def explain_saliency(model, data, target):
     viz.visualize_image_attr(attr, data.permute(1, 2, 0).numpy(), method="heat_map", title="Saliency Attribution")
 
 def explain_integrated_gradients(model, data, target, baseline=None):
+    model.double()
+    model.to(device)
     model.eval()
     integrated_gradients = IntegratedGradients(model)
     attr = integrated_gradients.attribute(data, baseline, target=target, n_steps=50)
@@ -107,6 +162,16 @@ def explain_integrated_gradients(model, data, target, baseline=None):
 
 if __name__ == "__main__":
     loading_state_dict()
-    explain_gradCAM(model=model_CNN_cifar, data=data_CNN_cifar.data[1], target=data_CNN_cifar.targets[1], layer=model_CNN_cifar.conv0)
+    #explain_gradCAM(model=model_CNN_cifar, data=data_CNN_cifar.data[1], target=data_CNN_cifar.targets[1], layer=model_CNN_cifar.conv0)
+    #explain_gradCAM(model=model_CNN_mnist, data=data_CNN_mnist.data[1], target=data_CNN_mnist.targets[1], layer=model_CNN_mnist.conv0)
+
+    '''
+    DZIAŁA MODEL - ponizej funkcja do sprawdzania ewaluacji modelu
+
+    execute_model(data_set=data_CNN_cifar, model=model_CNN_cifar, data_name='CNN_cifar')
+    '''
+
+
+    explain_saliency(model=model_CNN_cifar, data=data_CNN_cifar.data[1], target=data_CNN_cifar.targets[1])
 
     
