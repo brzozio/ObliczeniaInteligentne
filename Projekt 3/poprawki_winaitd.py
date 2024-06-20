651b393f0e0c6095ad6fbe5980e355f694998cdc
brzozio <michal.brzozowski@interia.eu>
2024-06-20 17:14:13 +0200

    finished violin plots

--- Projekt 3/poprawki_winaitd.py
+++ Projekt 3/poprawki_winaitd.py
@@ -147,7 +147,7 @@ def visualize_attributions(attributions, input_tensor, model_name, method=None, 
 
         df = pd.DataFrame(data, columns=["Feature", "Attribution"])
 
-        sb.violinplot(x="Feature", y="Attribution", data=df, ax=ax, scale="width")
+        sb.violinplot(x="Feature", y="Attribution", data=df, ax=ax, scale="width", palette=sb.color_palette("husl", len(features)))
         ax.set_ylabel('Attribution value')
 
         ax.set_title(f"Class: {classes[target]}", fontname='Arial', fontsize=16, fontweight='bold')
@@ -156,11 +156,8 @@ def visualize_attributions(attributions, input_tensor, model_name, method=None, 
         
 
 
-def explain_MLP(model=None, data_set=None, model_name=None, features=None, classes=None):
+def explain_MLP(model=None, data_set=None, model_name=None, features=None, classes=None, method=None):
     _, ax = plt.subplots(len(classes), 1, figsize=(10, 5))
-    attr, _, _ = get_attributions(model=model, input_tensor=data_set.data, target_class=data_set.targets, method="integrated_gradients")
-    y_min = np.min(attr.cpu().detach().numpy()) - 5
-    y_max = np.max(attr.cpu().detach().numpy()) + 5
 
     for target in range(len(classes)):
         indices = []
@@ -172,21 +169,27 @@ def explain_MLP(model=None, data_set=None, model_name=None, features=None, class
         data_tensor = data_set.data[indices]
         targets = data_set.targets[indices]
         
-        int_grd, input_tensor, target_tensor  = get_attributions(model=model, input_tensor=data_tensor, target_class=targets, method="integrated_gradients")
-        visualize_attributions(int_grd, input_tensor, model_name=model_name,  method="integrated_gradients_barplot", target_tensor=target_tensor, features=features, target=target, classes=classes, ax=ax[target])
-        ax[target].set_ylim([y_min, y_max])
-        if target < len(classes) - 1: ax[target].tick_params(axis='x',which='both',bottom=False,top=False,labelbottom=False)
+        int_grd, input_tensor, target_tensor  = get_attributions(model=model, input_tensor=data_tensor, target_class=targets, method=method)
+        visualize_attributions(int_grd, input_tensor, model_name=model_name,  method=f"{method}_barplot", target_tensor=target_tensor, features=features, target=target, classes=classes, ax=ax[target])
+        if target < len(classes) - 1: 
+            ax[target].tick_params(axis='x',which='both',bottom=False,top=False,labelbottom=False)
+            ax[target].set_xlabel('')
+
         if len(features) != 4: ax[target].tick_params(axis='x', rotation=90)
+        attr, _, _ = get_attributions(model=model, input_tensor=data_set.data, target_class=data_set.targets, method=method)
+        y_min = np.min(attr.cpu().detach().numpy()) - 5
+        y_max = np.max(attr.cpu().detach().numpy()) + 5
+        ax[target].set_ylim([y_min, y_max])
 
-
+    plt.suptitle(f"{model_name.split()[1]} Dataset - {method} Attribution Distribution", fontsize=20, fontname="Arial", fontweight="bold")
     plt.show()
     
 
 if __name__ == "__main__":
     
     loading_state_dict()
-    explain_MLP(model=model_MLP_iris, data_set=data_MLP_iris, model_name="MLP Iris", features=iris_features, classes=iris_classes)
-    explain_MLP(model=model_MLP_wine, data_set=data_MLP_wine, model_name="MLP Wine", features=wine_features, classes=wine_classes)
-    explain_MLP(model=model_MLP_breast_cancer, data_set=data_MLP_breast_cancer, model_name="MLP Breast", features=breast_cancer_features, classes=breast_cancer_classes)
+    explain_MLP(model=model_MLP_iris, data_set=data_MLP_iris, model_name="MLP Iris", features=iris_features, classes=iris_classes, method="integrated_gradients")
+    explain_MLP(model=model_MLP_wine, data_set=data_MLP_wine, model_name="MLP Wine", features=wine_features, classes=wine_classes, method="integrated_gradients")
+    explain_MLP(model=model_MLP_breast_cancer, data_set=data_MLP_breast_cancer, model_name="MLP Breast", features=breast_cancer_features, classes=breast_cancer_classes, method="integrated_gradients")
  
   
