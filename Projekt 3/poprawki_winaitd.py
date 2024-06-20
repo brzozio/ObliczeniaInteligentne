a04e58ca5b40f5b9ba134af0bf40585e6e2c6d03
brzozio <michal.brzozowski@interia.eu>
2024-06-20 16:55:11 +0200

    finished violinplots

--- Projekt 3/poprawki_winaitd.py
+++ Projekt 3/poprawki_winaitd.py
@@ -135,11 +135,10 @@ def tensor_to_attribution_heatmap(tensor):
     out = out[0]
     return out
 
-def visualize_attributions(attributions, input_tensor, model_name, method=None, target_tensor=None, features=None, target=None, classes=None):
+def visualize_attributions(attributions, input_tensor, model_name, method=None, target_tensor=None, features=None, target=None, classes=None, ax=None):
     matplotlib.rcParams.update({'font.size': 7})
     
     if method == "saliency_barplot" or method == "integrated_gradients_barplot":
-        _, ax = plt.subplots(1, 1, figsize=(10, 5))
         data = []
 
         for i, feature in enumerate(features):
@@ -150,44 +149,44 @@ def visualize_attributions(attributions, input_tensor, model_name, method=None, 
 
         sb.violinplot(x="Feature", y="Attribution", data=df, ax=ax, scale="width")
         ax.set_ylabel('Attribution value')
-        ax.set_ylim()
-        ax.set_title(f'{features}', fontsize=12, fontweight='bold')
 
-        plt.suptitle(f"Class: {classes[target]}", fontname='Arial', fontsize=30, fontweight='bold')
+        ax.set_title(f"Class: {classes[target]}", fontname='Arial', fontsize=16, fontweight='bold')
+        
         plt.tight_layout()
-        plt.show()
+        
 
 
-def explain_MLP(model=None, data_set=None, model_name=None, target=None, features=None, classes=None):
+def explain_MLP(model=None, data_set=None, model_name=None, features=None, classes=None):
+    _, ax = plt.subplots(len(classes), 1, figsize=(10, 5))
+    attr, _, _ = get_attributions(model=model, input_tensor=data_set.data, target_class=data_set.targets, method="integrated_gradients")
+    y_min = np.min(attr.cpu().detach().numpy()) - 5
+    y_max = np.max(attr.cpu().detach().numpy()) + 5
 
-    indices = []
+    for target in range(len(classes)):
+        indices = []
 
-    for idx, label in enumerate(data_MLP_iris.targets):
-        if label == target:
-            indices.append(idx)
-    data_tensor = data_set.data[indices]
-    targets = data_set.targets[indices]
-    
-    #saliency_attr, input_tensor, target_tensor = get_attributions(model=model, input_tensor=data_tensor, target_class=targets, method="saliency")
-    #visualize_attributions(saliency_attr, input_tensor=input_tensor, model_name=model_name,  method="saliency_barplot", target_tensor=target_tensor, features=features, target=target)
+        for idx, label in enumerate(data_set.targets):
+            if label == target:
+                indices.append(idx)
 
-    int_grd, input_tensor, target_tensor  = get_attributions(model=model, input_tensor=data_tensor, target_class=targets, method="integrated_gradients")
-    visualize_attributions(int_grd, input_tensor, model_name=model_name,  method="integrated_gradients_barplot", target_tensor=target_tensor, features=features, target=target, classes=classes)
+        data_tensor = data_set.data[indices]
+        targets = data_set.targets[indices]
+        
+        int_grd, input_tensor, target_tensor  = get_attributions(model=model, input_tensor=data_tensor, target_class=targets, method="integrated_gradients")
+        visualize_attributions(int_grd, input_tensor, model_name=model_name,  method="integrated_gradients_barplot", target_tensor=target_tensor, features=features, target=target, classes=classes, ax=ax[target])
+        ax[target].set_ylim([y_min, y_max])
+        if target < len(classes) - 1: ax[target].tick_params(axis='x',which='both',bottom=False,top=False,labelbottom=False)
+        if len(features) != 4: ax[target].tick_params(axis='x', rotation=90)
+
+
+    plt.show()
     
 
 if __name__ == "__main__":
     
     loading_state_dict()
-    explain_MLP(model=model_MLP_iris, data_set=data_MLP_iris, model_name="MLP Iris", target=0, features=iris_features, classes=iris_classes)
-    explain_MLP(model=model_MLP_iris, data_set=data_MLP_iris, model_name="MLP Iris", target=1, features=iris_features, classes=iris_classes)
-    explain_MLP(model=model_MLP_iris, data_set=data_MLP_iris, model_name="MLP Iris", target=2, features=iris_features, classes=iris_classes)
-
-    explain_MLP(model=model_MLP_wine, data_set=data_MLP_wine, model_name="MLP Wine", target=0, features=wine_features, classes=wine_classes)
-    explain_MLP(model=model_MLP_wine, data_set=data_MLP_wine, model_name="MLP Wine", target=1, features=wine_features, classes=wine_classes)
-    explain_MLP(model=model_MLP_wine, data_set=data_MLP_wine, model_name="MLP Wine", target=2, features=wine_features, classes=wine_classes)
-    
-    explain_MLP(model=model_MLP_breast_cancer, data_set=data_MLP_breast_cancer, model_name="MLP Breast", target=0, features=breast_cancer_features, classes=breast_cancer_classes)
-    explain_MLP(model=model_MLP_breast_cancer, data_set=data_MLP_breast_cancer, model_name="MLP Breast", target=1, features=breast_cancer_features, classes=breast_cancer_classes)
-
+    explain_MLP(model=model_MLP_iris, data_set=data_MLP_iris, model_name="MLP Iris", features=iris_features, classes=iris_classes)
+    explain_MLP(model=model_MLP_wine, data_set=data_MLP_wine, model_name="MLP Wine", features=wine_features, classes=wine_classes)
+    explain_MLP(model=model_MLP_breast_cancer, data_set=data_MLP_breast_cancer, model_name="MLP Breast", features=breast_cancer_features, classes=breast_cancer_classes)
  
   
